0c54bbb18bc131f881ce60686dc668c242675e3e
Mubby007 <mubarakadebayo06@gmail.com>
2026-09-08 23:18:56 +0100

    mhizzy

--- app/data.ts
+++ app/data.ts
@@ -1,4 +1,4 @@
-import lottery from "./asset/teddy.png";
+import lottery from "./asset/mhizzy.jpeg";
 import donation from "./asset/donation.jpeg";
 import Aa from "./asset/A.jpeg";
 import Bb from "./asset/B.jpeg";
